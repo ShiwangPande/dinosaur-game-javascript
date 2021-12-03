f6fc85564f6283a527045c919b30c530da760ef0
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2021-12-03 01:46:28 -0300

    Finished project
    - Insert the validation of the jump
    - Update the elements (background, character and obstacle)
    - Position the game at the center
    - Create the title screen
    - Create the "Game Over" screen
    - Create the button "Try Again"
    - Insert the feature to start/restart the game
    - Insert the footer

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Dinosaur Game Project
 
 Recreation of the Dinosaur Game from Google Chrome web browser, using HTML, CSS and JavaScript.
-
-Project under development.
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const character = document.querySelector('.character');
 const gameMap = document.querySelector('.game');
 let isJumping = false;
+let inGame = false;
+let position = 0;
 
 function handleKeyUp(event){
     if(event.keyCode === 32){
@@ -11,7 +13,6 @@ function handleKeyUp(event){
 }
 
 function jump(){
-    let position = 0;
     isJumping = true;
     let upInterval = setInterval(() => {
         if (position >= 150){
@@ -39,10 +40,71 @@ function jump(){
 function createObstacle(){
     const obstacle = document.createElement('div');
     let obstaclePosition = 1000;
+    let randomTime = Math.random() * 10000;
+
     obstacle.classList.add('obstacle');
     obstacle.style.left = 1000 + 'px';
     gameMap.appendChild(obstacle);
+
+    let leftInterval = setInterval(() => {
+        if(obstaclePosition < -60){
+            // Remove past obstacles
+            clearInterval(leftInterval);
+            gameMap.removeChild(obstacle);
+        } else if (obstaclePosition > 0 && obstaclePosition < 90 && position < 90) {
+            // Game Over
+
+            // Remove additional obstacles
+            for(let i = 1; i < document.querySelectorAll('.obstacle').length; i++){
+                gameMap.removeChild(document.querySelectorAll('.obstacle')[i]);
+            }
+            
+            clearInterval(leftInterval);
+            leftInterval = null;
+            clearTimeout(newObstacles);
+            newObstacles = null;
+            
+            // Print the 'Game Over' screen
+            if(inGame){
+                inGame = false;
+                gameMap.classList.add('over');
+                let gameOver = document.createElement('h1');
+                gameOver.innerHTML = 'Game Over';
+                
+                let retry = document.createElement('button');
+                retry.classList.add('action');
+                retry.innerHTML = 'Play Again?';
+    
+                gameMap.appendChild(gameOver);
+                gameMap.appendChild(retry);
+    
+                document.querySelector('.action').addEventListener('click', startGame);
+            }
+        } else {
+            // Move obstacles
+            obstaclePosition -= 10;
+            obstacle.style.left = obstaclePosition + 'px';
+        }
+    }, 20);
+
+    let newObstacles = setTimeout(createObstacle, randomTime);
 }
 
-createObstacle();
-document.addEventListener('keyup', handleKeyUp);
+// Remove splash screen and start a new game
+function startGame(){
+    inGame = true;
+    if(gameMap.classList.contains('over')){
+        gameMap.removeChild(document.querySelector('h1'));
+        gameMap.removeChild(document.querySelector('.action'));
+
+        let obstacle = document.querySelector('.obstacle');
+        if(obstacle){
+            gameMap.removeChild(obstacle);
+        }
+        gameMap.classList.remove('over');
+    }
+    createObstacle();
+}
+
+document.addEventListener('keydown', handleKeyUp);
+document.querySelector('.action').addEventListener('click', startGame);
