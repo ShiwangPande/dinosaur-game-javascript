0f751176397d6eaf5cd1f6788ab1624a788acdce
Shiwang Pandey <110737840+ShiwangPande@users.noreply.github.com>
2022-09-12 02:27:17 +0530

    do it

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+
+var myGamePiece;
+var myObstacles = [];
+var myScore;
+
+function startGame() {
+    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    myGameArea.start();
+}
+
+
 function jump() {
     if (dispatchEvent.classList != "jump") {
         //first it checks if the dino is mid-jump. If not, it makes it jump.
@@ -7,7 +18,7 @@ function jump() {
         setTimeout(function () {
             dino.classList.remove("jump");
             //removes the jump class from the dino once it has jumped so that it can jump again
-        }, 500);
+        }, 300);
     }
 }
 let checkAlive = setInterval(function () {
@@ -18,7 +29,7 @@ let checkAlive = setInterval(function () {
         window.getComputedStyle(cactus).getPropertyValue("left")
     );
     //check for collision
-    if (cactusLeft > 0 && cactusLeft < 40 && dinoTop >= 140) {
+    if (cactusLeft > 0 && cactusLeft < 45 && dinoTop >= 110) {
         dino.style.animationPlayState = "paused";
         cactus.style.animationPlayState = "paused";
         alert("Whoops! Game Over :(");
@@ -33,5 +44,34 @@ document.addEventListener("keydown", function (event) {
 // });
 function functionToExecute() {
     jump();
+}
+function timer() {
+    var sec = 30;
+    var timer = setInterval(function () {
+        document.getElementById('safeTimerDisplay').innerHTML = '00:' + sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    } else {
+        return valString;
+    }
 }
 {/* <b style="color: rgb(38, 38, 38); font-family: Poppins; font-size: x-large; --darkreader-inline-color:#d0ccc6;" data-darkreader-inline-color=""></b> */ }
