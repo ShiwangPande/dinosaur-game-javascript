4326c71bb68048cf9aa8cd39c0449a484f5cdf85
Shiwang Pandey <110737840+ShiwangPande@users.noreply.github.com>
2022-09-12 01:26:56 +0530

    do it

--- script.js
+++ script.js
@@ -1,110 +1,37 @@
-const character = document.querySelector('.character');
-const gameMap = document.querySelector('.game');
-let isJumping = false;
-let inGame = false;
-let position = 0;
-
-function handleKeyUp(event){
-    if(event.keyCode === 32){
-        if(!isJumping){
-            jump();
-        }
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+function jump() {
+    if (dispatchEvent.classList != "jump") {
+        //first it checks if the dino is mid-jump. If not, it makes it jump.
+        dino.classList.add("jump");
+        setTimeout(function () {
+            dino.classList.remove("jump");
+            //removes the jump class from the dino once it has jumped so that it can jump again
+        }, 500);
     }
 }
-
-function jump(){
-    isJumping = true;
-    let upInterval = setInterval(() => {
-        if (position >= 150){
-            // Apex
-            clearInterval(upInterval);
-
-            // Going down
-            let downInterval = setInterval(() => {
-                if(position <= 0){
-                    clearInterval(downInterval);
-                    isJumping = false;
-                } else {
-                    position -= 20;
-                    character.style.bottom = position + 'px';
-                }
-            }, 20);
-        } else {
-        // Going up
-            position += 20;
-            character.style.bottom = position + 'px';
-        }
-    }, 20);
-}
-
-function createObstacle(){
-    const obstacle = document.createElement('div');
-    let obstaclePosition = 1000;
-    let randomTime = Math.random() * 10000;
-
-    obstacle.classList.add('obstacle');
-    obstacle.style.left = 1000 + 'px';
-    gameMap.appendChild(obstacle);
-
-    let leftInterval = setInterval(() => {
-        if(obstaclePosition < -60){
-            // Remove past obstacles
-            clearInterval(leftInterval);
-            gameMap.removeChild(obstacle);
-        } else if (obstaclePosition > 0 && obstaclePosition < 90 && position < 90) {
-            // Game Over
-
-            // Remove additional obstacles
-            for(let i = 1; i < document.querySelectorAll('.obstacle').length; i++){
-                gameMap.removeChild(document.querySelectorAll('.obstacle')[i]);
-            }
-            
-            clearInterval(leftInterval);
-            leftInterval = null;
-            clearTimeout(newObstacles);
-            newObstacles = null;
-            
-            // Print the 'Game Over' screen
-            if(inGame){
-                inGame = false;
-                gameMap.classList.add('over');
-                let gameOver = document.createElement('h1');
-                gameOver.innerHTML = 'Game Over';
-                
-                let retry = document.createElement('button');
-                retry.classList.add('action');
-                retry.innerHTML = 'Play Again?';
-    
-                gameMap.appendChild(gameOver);
-                gameMap.appendChild(retry);
-    
-                document.querySelector('.action').addEventListener('click', startGame);
-            }
-        } else {
-            // Move obstacles
-            obstaclePosition -= 10;
-            obstacle.style.left = obstaclePosition + 'px';
-        }
-    }, 20);
-
-    let newObstacles = setTimeout(createObstacle, randomTime);
-}
-
-// Remove splash screen and start a new game
-function startGame(){
-    inGame = true;
-    if(gameMap.classList.contains('over')){
-        gameMap.removeChild(document.querySelector('h1'));
-        gameMap.removeChild(document.querySelector('.action'));
-
-        let obstacle = document.querySelector('.obstacle');
-        if(obstacle){
-            gameMap.removeChild(obstacle);
-        }
-        gameMap.classList.remove('over');
+let checkAlive = setInterval(function () {
+    let dinoTop = parseInt(
+        window.getComputedStyle(dino).getPropertyValue("top")
+    );
+    let cactusLeft = parseInt(
+        window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+    //check for collision
+    if (cactusLeft > 0 && cactusLeft < 40 && dinoTop >= 140) {
+        dino.style.animationPlayState = "paused";
+        cactus.style.animationPlayState = "paused";
+        alert("Whoops! Game Over :(");
+        window.location.reload();
     }
-    createObstacle();
+}, 10);
+document.addEventListener("keydown", function (event) {
+    jump();
+});
+// document.addEventListener("click", function (event) {
+//     jump();
+// });
+function functionToExecute() {
+    jump();
 }
-
-document.addEventListener('keydown', handleKeyUp);
-document.querySelector('.action').addEventListener('click', startGame);
+{/* <b style="color: rgb(38, 38, 38); font-family: Poppins; font-size: x-large; --darkreader-inline-color:#d0ccc6;" data-darkreader-inline-color=""></b> */ }
